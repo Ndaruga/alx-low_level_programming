4482778763e68b0f8f58d53ae539a265ac0effb3
ndarugaa <ndaruga.francis@gmail.com>
2023-03-21 13:23:48 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,41 @@
 
 
+/**
+ * _putchar - writes the character c to stdout
+ */
+
 int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
