7a69879b962fab0b4b492ce42814a9748e065fd1
ndarugaa <ndaruga.francis@gmail.com>
2023-03-23 04:44:43 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,12 +2,12 @@
 
 /**
  * positive_or_negative - checks for a negative or positve number
- * @i: character to check 
+ * @i: character to check
  * Return: char status (positive or negative)
  */
 
 void positive_or_negative(int i)
-{       
+{
 	if (i == 0)
 	{
 		printf("%d is zero\n", i);
