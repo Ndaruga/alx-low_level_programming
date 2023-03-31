c186d812bc61eeae46e80f0fe3db4d61e0cac099
Ndarugaa <ndaruga.francis@gamil.com>
2023-03-31 16:38:31 +0300

    fix betty style on str concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include "stdio.h"
 
+/**
+ * _strlen - returns the length of a string
+ * @s: Characters to count
+ * Return: count integer
+ */
+
 int _strlen(char *s)
 {
 	int count = 0;
@@ -15,21 +21,20 @@ int _strlen(char *s)
 
 /**
  * _strcat - concatenate two strings
- * @i: length of destination
- * @j: increment the source
+ * @dest: destination string
+ * @src: source string
  * Return: None
  */
 
 char *_strcat(char *dest, char *src)
 {
-    int j;
-    int i = _strlen(dest);
+	int j;
+	int i = _strlen(dest);
 
-    for (j = 0; src[j] != '\0'; i++, j++)
+	for (j = 0; src[j] != '\0'; i++, j++)
 		dest[i] = src[j];
 
 	dest[i] = '\0';
 
 	return (dest);
-
 }
