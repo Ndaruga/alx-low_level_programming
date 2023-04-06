36658639c270fcf82f64bea6a9b08997eb79790d
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-06 23:06:30 +0300

    output

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _print_rev_recursion - print a string in reverse using recusion
  * @s: character to be printed
@@ -8,10 +9,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 	}
-	_putchar(s[0]);
-	return ;
+	_putchar(*s);
+
 }
