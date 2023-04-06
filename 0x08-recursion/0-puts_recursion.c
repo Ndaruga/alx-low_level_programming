7565e1820d10902f0b34f01d754f2049949d3ad9
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-06 03:23:16 +0300

    creating puts fn

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: character to be printed
+ * Return: None
+*/
+
+void _puts_recursion(char *s)
+{
+    /* Get the length of the string passed*/
+    
+    if (s == "\0")
+        _putchar('\n');
+    _putchar(*s);
+    _puts_recursion(*++s);    
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,7 @@
+# C - Recursion
+
+## General objectives
+<li>What is recursion</li>
+<li>How to implement recursion</li>
+<li>In what situations you should implement recursion</li>
+<li>In what situations you shouldn’t implement recursion</li>
