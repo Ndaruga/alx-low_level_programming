c1cf552e1ee4e7638fb9f76083a382187bd9f955
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-08 01:26:30 +0300

    --

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-for file in ./*.c
+for file in *.c
 do
     gcc -c $file
+    ar rc liball.a "${file%.c}.o"
+    rm "${file%.c}.o"
 
 done
-
-# ************************************************#
-
-# create a library archive file from all the object files
-ar rcs liball.a *.o
-
-# remove the object files
-rm -f *.o
-
