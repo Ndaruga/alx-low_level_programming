88f8308147cb93642693a784248eb1eb56914288
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-11 00:56:39 +0300

    Function description

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include "stdio.h"
 # include "main.h"
 
+/**
+ * main - main function to print function name
+ * @argc: number of arguments in the function main
+ * @argv: holds an array of arguments
+ * Return: 0
+*/
+
 int main(int argc, char **argv)
 {
-    if (argc >= 1)
-        printf("%s\n",argv[0]);
-    return (0);
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	return (0);
 }
