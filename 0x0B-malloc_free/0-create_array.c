cd978302adc15c78c9b3dc1eb619c5b647d18bf2
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-13 18:43:40 +0300

    for loop

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	unsigned int i;
 
 	if (size == 0)
 		return NULL;
 	str = malloc(sizeof(c) * size);
 	if (str == NULL)
 		return NULL;
-	str[0] = c;
+	for (i = 0; i <= size; i++)
+		str[i] = c;
 	return (str);
 	free(str);
 }
