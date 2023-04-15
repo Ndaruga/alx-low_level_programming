05a0df8ce74cb7859bd84723dd4ede518561903d
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-15 14:11:34 +0300

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,13 @@
 
 char *_strdup(char *str)
 {
-    char *dup_str;
+	char *dup_str;
 	int i = 0;
 	int len = 0;
+	char *str_ptr = str; /*copy the string*/
 
 	if (str == NULL)
 	{
-		printf("String is null\n");
 		return (NULL);
 	}
 	/*---------Check length of the string without null terinator------*/
@@ -27,15 +27,18 @@ char *_strdup(char *str)
 		len++;
 	}
 	len++;
+	/*------While loop haas modifired the value of *str----*/
+
 	dup_str = malloc(sizeof(char) * len);
-	if (dup_str == NULL )
+	if (dup_str == NULL)
 	{
 		printf("Malloc returned NUll\n");
 		return (NULL);
 	}
-	for (i = 0; i < len; i++, *str++)
+
+	for (i = 0; i < len; i++, *str_ptr++)
 	{
-		dup_str[i] = *(str);
+		dup_str[i] = *str_ptr;
 	}
 	return (dup_str);
 }
