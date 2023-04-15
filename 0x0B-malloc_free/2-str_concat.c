fc9bb166805b38cb0d481cc81dce32a964b75b17
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-15 18:41:38 +0300

    check 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,51 +11,53 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    char *concatd_str;
-    int len_s1 = 0;
-    int len_s2 = 0;
-    int i = 0;
+	char *concatd_str;
+	int len_s1 = 0;
+	int len_s2 = 0;
+	int i = 0;
 
-    char *temp = s1; /*---- set a duplicate value of s1 to temp*/
+	char *temp = s1; /*---- set a duplicate value of s1 to temp*/
 
-    if (s1 == NULL)
-        return ("");
-    if (s2 == NULL)
-        return ("");
-    
-    /*-----Get the length of s1-----*/
-    while (*s1++)
-    {
-        len_s1++;
-    }
-    s1 = temp;
-    
-    /**-------Get length of s2--------*/
-    temp = s2;
+	if (s1 != NULL)
+	{
+		if (s2 != NULL)
+		{
+			/*-----Get the length of s1-----*/
+			while (*s1++)
+			{
+				len_s1++;
+			}
+			s1 = temp;
 
-    while (*s2++)
-    {
-        len_s2++;
-    }
-    len_s2++;
-    s2 = temp;
+			/**-------Get length of s2--------*/
+			temp = s2;
+
+			while (*s2++)
+			{
+				len_s2++;
+			}
+			len_s2++;
+			s2 = temp;
 
 
-    concatd_str = malloc(sizeof(char) * (len_s1 + len_s2));
-    if (concatd_str == NULL)
-        return (NULL);
+			concatd_str = malloc(sizeof(char) * (len_s1 + len_s2));
+			if (concatd_str == NULL)
+				return (NULL);
 
-    /*------Add s1 to concatd_str-------*/
-    for (i = 0; i < len_s1; i++, *s1++)
-    {
-        concatd_str[i] = *s1;
-    }
+			/*------Add s1 to concatd_str-------*/
+			for (i = 0; i < len_s1; i++, *s1++)
+			{
+				concatd_str[i] = *s1;
+			}
 
-     for (i = len_s1; i < (len_s1 + len_s2); i++, *s2++)
-    {
-        concatd_str[i] = *s2;
-    }
-    
-    return (concatd_str);
-    
+			for (i = len_s1; i < (len_s1 + len_s2); i++, *s2++)
+			{
+				concatd_str[i] = *s2;
+			}
+
+			return (concatd_str);
+		}
+		return "";
+	}
+	return "";
 }
