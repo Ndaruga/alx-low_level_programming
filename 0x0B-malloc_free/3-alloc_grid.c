fa4079d755424bf5804cdef2174715844bc13a66
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-16 20:52:02 +0300

    Add comments and free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,31 +11,35 @@
 
 int **alloc_grid(int height, int width)
 {
-    /**--------Decleare Variables ------*/
-    int **two_dim;
+    int **two_dim; /*----- Pointer to a pointer to an int-----*/
     int i, j;
 
     if (height <= 0)
         return (NULL);
     if (width <= 0)
         return (NULL);
-    
-    two_dim = malloc(sizeof(int*) * width);
+
+    /*-----Allocate memory of size int* to width. This way we allocate a memory 
+            block for a 1D array of pointers.-----*/
+    two_dim = (int **) malloc(sizeof(int*) * width);
     if (two_dim == NULL)
         return (NULL);
     
     /*--------Nested for loops -------*/
     for (i = 0; i < width; i++)
     {
+        /*--------For each Pointer in the 1D array, allocate other array
+                of pointers of size int thereby creating a 2D array----------*/
         two_dim[i] = (int *) malloc(sizeof(int) * height);
         if (two_dim[i] == NULL)
             return NULL;
         for (j = 0; j < height; j++)
         {
-            two_dim[i][j] = 0;
+            two_dim[i][j] = 0; /* ----Fill all elements in the 2D array----*/
         }
+        free(two_dim[i]);
     }
-
+    free(two_dim);
     return (two_dim);
 
 }
