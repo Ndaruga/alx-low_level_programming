0a6de15e1157fc4bb26b8775c8f9d8eecd503614
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-21 16:35:13 +0300

    define Macro PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,8 @@
+/*--- Next two lines (Conditional MACROS) prevent double inclusion--*/
+#ifndef _PI_H_
+#define _PI_H_
+
+/*---PI is a macro that is replaced by  3.141.....*/
+#define PI 3.14159265359
+
+#endif
