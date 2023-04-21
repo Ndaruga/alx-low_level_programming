38e7ca89df07bbfc0cc76f18e4be77a1ff316a77
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-21 16:37:47 +0300

    fix double inclusion protection

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,6 @@
-/*--- Next two lines (Conditional MACROS) prevent double inclusion--*/
 #ifndef _PI_H_
 #define _PI_H_
+/*--- Previous two lines (Conditional MACROS) prevent double inclusion--*/
 
 /*---PI is a macro that is replaced by  3.141.....*/
 #define PI 3.14159265359
