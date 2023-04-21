9e8970716f3b2716d7cc1606984f64bdf2323b90
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-21 18:56:35 +0300

    struct description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Name of the struct
+ * @name: Name of dog
+ * @age: floating number
+ * @owner: Owner
+*/
 typedef struct dog
 {
 	char *name;
