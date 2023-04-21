2efdd3651f4bc91d068288cc3087d9096d3c351e
Ndarugaa <ndaruga.francis@gmail.com>
2023-04-21 05:48:24 +0300

    Only files created

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * free_grid - frees the 2D grid prebiously created by alloc_grid
+ * @grid: grid shape
+ * @height: height of the grid
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+    
+}
