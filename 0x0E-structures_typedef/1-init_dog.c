921b6b5c2c539be630c9393186bd50a08ae6767f
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-22 14:46:07 +0300

    betty

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,8 +2,18 @@
 #include "dog.h"
 #include "stdlib.h"
 
+/**
+ * init_dog - function
+ * struct dog - structure of dog
+ * @d: the new dog
+ * @name: name of new dog
+ * @age: age of dog
+ * @owner: ownwer of dog
+*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 	{
 		return;
