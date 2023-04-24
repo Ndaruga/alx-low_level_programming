864eddca3ac3fc38589f01b7b996934619d23daa
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-25 01:07:21 +0300

    ---

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -15,11 +15,9 @@ void *malloc_checked(unsigned int b)
 	/**
 	 * allocate memory and exit eith status 98 if it fails
 	*/
-	alloc = malloc(sizeof(b));
+	alloc = malloc(b);
 	if (alloc == NULL)
-	{
 		exit(98);
-	}
 
 	return (alloc);
 }
