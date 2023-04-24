795edef13027a92e48da02fbaf5e82e0b7bee228
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-24 16:38:40 +0300

    clean up

--- 0x0B-malloc_free/4-main.c
+++ 0x0B-malloc_free/4-main.c
@@ -36,7 +36,18 @@ void print_grid(int **grid, int width, int height)
  */
 int main(void)
 {
-    free_grid(NULL, 0);
-    printf("OK\n");
+    int **grid;
+
+    grid = alloc_grid(6, 4);
+    if (grid == NULL)
+    {
+        return (1);
+    }
+    print_grid(grid, 6, 4);
+    printf("\n");
+    grid[0][3] = 98;
+    grid[3][4] = 402;
+    print_grid(grid, 6, 4);
+    free_grid(grid, 4);
     return (0);
 }
