5f7877c71e148650850ec1f4082ef4e5b5578504
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-25 00:37:01 +0300

    memory checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number to be allocated memory for
+ * Return: none
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *alloc;
+
+	/**
+	 * allocate memory and exit eith status 98 if it fails
+	*/
+	alloc = malloc(sizeof(int) * b);
+	if (alloc == NULL)
+	{
+		exit(98);
+	}
+
+	return (alloc);
+}
