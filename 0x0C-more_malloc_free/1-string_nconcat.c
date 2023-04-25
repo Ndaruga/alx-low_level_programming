5175cc7eee051c0684d58ce65b7e9e84ad815889
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-25 13:06:11 +0300

    fix string null

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,9 +31,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	num = n;
 
-	if ((s1 == NULL) || (s2 == NULL))/* account for NULL strings */
+	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
-
+	if (s2 == NULL)
+		s2 = "";
 	if (num < 0) /* account for negative n bytes */
 		return (NULL);
 	if (num >= _strlen(s2)) /* account for n too big */
