a0749543b1bdd3ee8809c48c42697a5a28f26af3
Ndarugaa <ndaruga.francis@gmail.com>
2023-04-28 02:26:46 +0300

    fix betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  */
 
 /* made possible by prior declaration of "__attribute__ ((constructor)) */
-void __attribute__ ((constructor)) run_before_main() 
+void __attribute__ ((constructor)) run_before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
