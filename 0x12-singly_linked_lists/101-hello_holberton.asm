a850b028f6810cd7ecea9b364225e098128cdc66
Ndarugaa <ndaruga.francis@gmail.com>
2023-04-28 02:33:50 +0300

    fix indentation and spaces

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
 section .data
-    msg db "Hello, Holberton", 0x0a, 0
+	msg db "Hello, Holberton", 0x0a, 0
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    push rbp
-    mov rbp, rsp
-    
-    lea rdi, [msg]
-    mov eax, 0
-    call printf
-    
-    mov rsp, rbp
-    pop rbp
-    ret
+	push rbp
+	mov rbp, rsp
+
+	lea rdi, [msg]
+	mov eax, 0
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+	ret
