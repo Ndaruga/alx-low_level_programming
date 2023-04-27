d56d2b3095dc0f9c99e65c5682e815a72d71044c
Ndarugaa <ndaruga.francis@gmail.com>
2023-04-28 02:25:52 +0300

    Execute fun before main fn

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * run_before_main - executes this function before the main function
+ */
+
+/* made possible by prior declaration of "__attribute__ ((constructor)) */
+void __attribute__ ((constructor)) run_before_main() 
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
