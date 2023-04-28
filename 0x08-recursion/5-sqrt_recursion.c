bd5a31383ee57e43dcfb517168bf64e302c6dbe5
Ndarugaa <ndaruga.francis@gmail.com>
2023-04-29 02:15:36 +0300

    returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
-/*******************************************************************************
- * _pow_recursion - print x to the power of y
- * @x: base integer
- * @y: power integer
- * Return: x**y
-*/
 
-int _pow_recursion(int x, int y)
+/**
+ * find_root - find square root of n, starting from the smallest possible, 0
+ * @n: n
+ * @root: test this root
+ * Return: natural square root, or -1 if not natural root
+ */
+
+int find_root(int n, int root)
 {
-	if (y > 0)
-		return (x * _pow_recursion(x, y - 1));
-	if (y == 0)
-		return (1);
-	return (-1);
+	if (root * root > n)
+		return (-1);
 
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
 }
-/**********************************************************************************/
 
-/** 
- * 
- * _sqrt_recursion - return natural square root 
- * @n: number to get the square root
- * Return: square root
-*/
+/**
+ * _sqrt_recursion - find square root of n
+ * @n: n
+ * Return: square root, or -1 if not natural root
+ */
 
 int _sqrt_recursion(int n)
 {
-    if (n <= 0)
-        return (-1);
-    return (_pow_recursion(n, 0.5));
+	if (n < 0)
+		return (-1);
+
+	return (find_root(n, 0));
 }
